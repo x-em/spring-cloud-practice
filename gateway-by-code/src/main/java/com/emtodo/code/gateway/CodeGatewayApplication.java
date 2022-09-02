d35ea3b823e10e1fcd36e3fe2d3eeea8830dbde6
emtodo <1521954794@qq.com>
2022-09-02 15:08:27 +0800

    update gateway style module

---
+++ gateway-by-code/src/main/java/com/emtodo/code/gateway/CodeGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.emtodo.code.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * CodeGateway 启动类
+ *
+ * @author em
+ */
+@SpringBootApplication
+public class CodeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGatewayApplication.class,args);
+    }
+}
---
+++ gateway-nacos-config/src/main/java/com/emtodo/nacos/gateway/NacosGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.emtodo.nacos.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * NacosGateway 启动类
+ *
+ * @author em
+ */
+@SpringBootApplication
+public class NacosGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosGatewayApplication.class,args);
+    }
+}
